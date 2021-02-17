91826230a86a4d603bab942e80ab2126ebe69098
daikisudo <kanigiusa2032@gmail.com>
2021-02-18 04:40:37 +0900

    配列2まで完了

---
+++ Lesson08_02.java
@@ -0,0 +1,9 @@
+public class Lesson08_02 {
+    public static void main(String[] args){
+        String[] arr = {"sato","suzuki","tanaahshi"};
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+    }
+}
